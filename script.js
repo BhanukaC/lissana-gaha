2b7d067432ed38b7142348e165ba72b8dc368840
BhanukaC <50510335+BhanukaC@users.noreply.github.com>
2021-04-01 12:42:03 +0530

    fixed bug

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
 
 var myGamePiece;
 var myScore;
-var clicks=0;
+var clicks = 0;
 
 
 
 
 function startGame() {
     myGameArea.start();
-    myGamePiece = new component(40, 90, "person.png", 275, 450,"image");
+    myGamePiece = new component(40, 90, "person.png", 275, 450, "image");
     myScore = new component("30px", "Consolas", "black", 450, 40, "text");
 }
 
 var myGameArea = {
-    canvas : document.createElement("canvas"),
-    start : function() {
-        this.canvas.width =650;
+    canvas: document.createElement("canvas"),
+    start: function () {
+        this.canvas.width = 650;
         this.canvas.height = 450;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-        this.interval = setInterval(updateGameArea, 10);  
+        this.interval = setInterval(updateGameArea, 10);
     },
-    stop : function() {
+    stop: function () {
         clearInterval(this.interval);
-        document.getElementById("marks").innerText = "Winner\n"+clicks+" tries";
-    },    
-    clear : function() {
+        document.getElementById("marks").innerText = "Winner\n" + clicks + " tries";
+    },
+    clear: function () {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     }
 }
@@ -39,37 +39,37 @@ function component(width, height, color, x, y, type) {
     this.width = width;
     this.height = height;
     this.x = x;
-    this.y = y;    
+    this.y = y;
     this.speedX = 0;
-    this.speedY = 0;    
+    this.speedY = 0;
     this.gravity = 0.05;
     this.gravitySpeed = 0;
-    this.update = function() {
+    this.update = function () {
         ctx = myGameArea.context;
         if (type == "image") {
-            ctx.drawImage(this.image, 
-                this.x, 
+            ctx.drawImage(this.image,
+                this.x,
                 this.y,
                 this.width, this.height);
-        
+
         }
         else if (this.type == "text") {
             ctx.font = this.width + " " + this.height;
             ctx.fillStyle = color;
             ctx.fillText(this.text, this.x, this.y);
         }
-         else {
+        else {
             ctx.fillStyle = color;
             ctx.fillRect(this.x, this.y, this.width, this.height);
         }
     }
-    this.newPos = function() {
+    this.newPos = function () {
         this.gravitySpeed += this.gravity;
         this.x += this.speedX;
         this.y += this.speedY + this.gravitySpeed;
         this.hitBottom();
     }
-    this.hitBottom = function() {
+    this.hitBottom = function () {
         var rockbottom = myGameArea.canvas.height - this.height;
         if (this.y > rockbottom) {
             this.y = rockbottom;
@@ -79,22 +79,22 @@ function component(width, height, color, x, y, type) {
 }
 
 function updateGameArea() {
-    myGamePiece.gravity=0.05;
+    myGamePiece.gravity = 0.05;
     myGameArea.clear();
-    myScore.text="Trys : " + clicks;
+    myScore.text = "Trys : " + clicks;
     myScore.update();
-    if(myGamePiece.y<=70){
+    if (myGamePiece.y <= 70) {
         myGameArea.stop();
     }
-    else{
+    else {
         myGamePiece.newPos();
     }
     myGamePiece.update();
 }
 
-function accelerate(n) {
+function accelerate() {
     clicks++;
-    myGamePiece.gravitySpeed  = n;
+    myGamePiece.gravitySpeed = -0.7;//speed increase by a click
 }
 
 
